ac6c93d9cd47d6aabb2190c6063af234e5323eed
Thanh Tung <tungnt14@vng.com.vn>
2019-10-02 14:20:57 +0700

    routing identity

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,17 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import { Home, NotfoundPage, Helper } from './components/MainPage';
 import { Creation, Detail } from './components/Campaign';
 import {Wallet} from './components/Wallet';
-// import IdentityUser from './components/IdentityUser';
-// import CheckingIdentity from './components/CheckingIdentity';
-// import Notification from './components/Notification';
+import IdentityUser from './components/Identity/IdentityUser';
+import CheckingIdentity from './components/Identity/CheckingIdentity';
+import Notification from './components/Identity/Notification';
 const Router = () =>
     <Switch>
         <Route path='/wallet' exact component={Wallet} />
         <Route path='/create' exact component={Creation} />
         <Route path='/help' exact component={Helper} />
-        {/* <Route path='/identity' exact component={IdentityUser} />
+        <Route path='/identity' exact component={IdentityUser} />
         <Route path='/checknew' exact component={CheckingIdentity} />
-        <Route path='/notification' exact component={Notification} /> */}
+        <Route path='/notification' exact component={Notification} />
         <Route path='/campaign/:id' exact component={Detail} />
         <Route path='/' exact component={Home} />
         <Route component={NotfoundPage} />
